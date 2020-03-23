eef329fe30c41d4c375a73cfcf0795c0508cb49b
nananan1117 <vfu_ya@mail.ru>
2020-03-23 12:09:10 +0700

    ProductCatalog

--- WindowsProject1/WindowsProject1/domain/ProductCatalog.h
+++ WindowsProject1/WindowsProject1/domain/ProductCatalog.h
@@ -1,5 +1,10 @@
 #pragma once
+#include "ItemId.h"
+#include "ProductDescription.h"
+#include <map>;
 class ProductCatalog
 {
-
+private:
+	std::map<ItemId, ProductDescription> Map;
+	std::map<ItemId, ProductDescription> description;
 };
